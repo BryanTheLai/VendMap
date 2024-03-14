feb486257fbfcfd1d5155862c61e80eb820048cf
Kevin <139090090+kevin07212004@users.noreply.github.com>
2024-03-14 12:36:15 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/vendmap/MainActivity.kt
+++ app/src/main/java/com/example/vendmap/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android Hello Kevin")
+                    Greeting("Android Hello Kevin 123 changes")
                 }
             }
         }
