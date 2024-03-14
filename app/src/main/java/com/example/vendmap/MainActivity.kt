45117357dc061c55db66bfe7b745c126a1204425
NotABot <135363764+BryanTheLai@users.noreply.github.com>
2024-03-14 13:08:52 +0800

    first terminal commit

--- app/src/main/java/com/example/vendmap/MainActivity.kt
+++ app/src/main/java/com/example/vendmap/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android Hello Kevin 123 changes 456 890")
+                    Greeting("Bryan 1:06pm")
                 }
             }
         }
